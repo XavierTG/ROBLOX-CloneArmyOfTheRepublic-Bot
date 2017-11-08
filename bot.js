7a5f38caf1fe4c82060f9829f6ffb54b0027a5d3
XavierTG <33395394+XavierTG@users.noreply.github.com>
2017-11-07 23:05:56 -0500

    Uploaded main bot code for early testing purposes.

---
+++ bot.js
@@ -0,0 +1,22 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  let link = client.generateInvite(["ADMINISTRATOR"])
+  console.log(link)
+});
+
+client.on('message', msg => {
+  if (msg.content === '-CAR-test') {
+    msg.reply('This is a test response to a test prompt message.'
+    return;
+  }
+  if (msg.content === '-CAR-botinfo') {
+    msg.reply('This discord bot is scripted and managed by XavierTG for usage by the ROBLOX group: -Clone Army Of The Republic-. This bot is in pre-alpha testing and is lacking many features at the moment. For more information, visit the partially complete bot concept document (will be updated every other day or daily) at https://github.com/XavierTG/ROBLOX-CloneArmyOfTheRepublic-Bot/blob/master/BotConcept.md'
+    return;
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
