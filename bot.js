59dd47a98e34672a3c9947329ae91fcbaead374c
XavierTG <33395394+XavierTG@users.noreply.github.com>
2017-11-07 23:12:18 -0500

    Corrected some errors.

--- bot.js
+++ bot.js
@@ -4,17 +4,17 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  let link = client.generateInvite(["ADMINISTRATOR"])
-  console.log(link)
+  let link = await client.generateInvite(["ADMINISTRATOR"]);
+  console.log(link);
 });
 
 client.on('message', msg => {
   if (msg.content === '-CAR-test') {
-    msg.reply('This is a test response to a test prompt message.'
+    msg.reply('This is a test response to a test prompt message.');
     return;
   }
   if (msg.content === '-CAR-botinfo') {
-    msg.reply('This discord bot is scripted and managed by XavierTG for usage by the ROBLOX group: -Clone Army Of The Republic-. This bot is in pre-alpha testing and is lacking many features at the moment. For more information, visit the partially complete bot concept document (will be updated every other day or daily) at https://github.com/XavierTG/ROBLOX-CloneArmyOfTheRepublic-Bot/blob/master/BotConcept.md'
+    msg.reply('This discord bot is scripted and managed by XavierTG for usage by the ROBLOX group: -Clone Army Of The Republic-. This bot is in pre-alpha testing and is lacking many features at the moment. For more information, visit the partially complete bot concept document (will be updated every other day or daily) at https://github.com/XavierTG/ROBLOX-CloneArmyOfTheRepublic-Bot/blob/master/BotConcept.md');
     return;
   }
 });
