f7371de1e6f5abeb2cc55965ed8c27d39a837c07
XavierTG <33395394+XavierTG@users.noreply.github.com>
2017-11-08 08:39:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.author.bot) return;
   if (msg.channel.type === "dm") return msg.channel.sendMessage("Sorry, but I am currently not capable of responding to DMs.");
-  console.log(`${msg.author.username} sent "${msg.content}" in ${msg.channel.name}`);
+  console.log(`${msg.author.username} sent "${msg.content}" in #${msg.channel.name}`);
   let channel = msg.channel.guild.channels.find(`name`, `logs`);
   channel.sendMessage(`${msg.author.username} sent "${msg.content}" in ${msg.channel.name}`);
   if (msg.content === '-CAR-test') {
